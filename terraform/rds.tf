0c27b4812e8d0756a01d982d41d2cef39693c29a
Alexandre Paiva <alexandremarcio1504@gmail.com>
2024-09-28 14:46:19 -0300

    multi_az BD

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -12,6 +12,7 @@ resource "aws_db_instance" "bd_wordpress" {
   skip_final_snapshot    = true
   db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.allow_rds.id]
+  multi_az = true
 
   lifecycle {
     ignore_changes = [password]
